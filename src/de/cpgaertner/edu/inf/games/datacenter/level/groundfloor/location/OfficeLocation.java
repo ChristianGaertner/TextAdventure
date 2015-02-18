3cee1c375dae2b19280feaa5d7750082b5f1c84d
Christian Gärtner <chrisdagardner@gmail.com>
2015-02-18 16:24:04 +0100

    Added some more basic Locations

---
+++ src/de/cpgaertner/edu/inf/games/datacenter/level/groundfloor/location/OfficeLocation.java
@@ -0,0 +1,6 @@
+package de.cpgaertner.edu.inf.games.datacenter.level.groundfloor.location;
+
+import de.cpgaertner.edu.inf.api.level.BaseLocation;
+
+public class OfficeLocation extends BaseLocation {
+}
