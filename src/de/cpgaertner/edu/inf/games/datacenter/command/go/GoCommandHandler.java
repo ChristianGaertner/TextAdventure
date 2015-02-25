53cf97b707ad7e8762c22c4813c3ada3ef321059
Christian Gärtner <chrisdagardner@gmail.com>
2015-02-25 15:43:26 +0100

    Improved go command

--- src/de/cpgaertner/edu/inf/games/datacenter/command/go/GoCommandHandler.java
+++ src/de/cpgaertner/edu/inf/games/datacenter/command/go/GoCommandHandler.java
@@ -2,6 +2,7 @@ package de.cpgaertner.edu.inf.games.datacenter.command.go;
 
 import de.cpgaertner.edu.inf.api.command.handler.CommandHandler;
 import de.cpgaertner.edu.inf.api.level.Coordinate;
+import de.cpgaertner.edu.inf.api.level.Location;
 import de.cpgaertner.edu.inf.api.level.player.Player;
 
 import java.io.IOException;
@@ -13,13 +14,25 @@ public class GoCommandHandler implements CommandHandler<GoCommand> {
         assert cmd != null;
 
 
-        Coordinate newPos = player.getPosition().get(cmd.getDirection());
+        Location.Direction dir = cmd.getDirection();
 
-        if (player.getLevel().getAt(newPos).isWalkable()) {
-            player.setPosition(newPos);
-        } else {
-            cmd.respondf("You cannot walk into the location in the %s", cmd.getDirection());
+        // First check the sides of the location itself
+        Location loc = player.getLevel().getAt(player.getPosition()).get(dir);
+
+        if (loc == null) {
+            Coordinate newPos = player.getPosition().get(cmd.getDirection());
+            Location newLoc = player.getLevel().getAt(newPos);
+            if (newLoc !=  null) {
+                if (newLoc.isWalkable()) {
+                    player.setPosition(newPos);
+                    cmd.respondf("Now you are at %s, this is a location of the type '%s'.",
+                            newPos,
+                            newLoc.getClass().getSimpleName()
+                    );
+                }
+            }
         }
 
+        cmd.respondf("You cannot walk into the location in the %s", cmd.getDirection());
     }
 }
