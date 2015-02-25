fd651517ca885acbdfcc0e8e8325aeb5d31a526c
Christian Gärtner <chrisdagardner@gmail.com>
2015-02-25 17:02:43 +0100

    Added basic door interaction

--- src/de/cpgaertner/edu/inf/api/routine/InteractionRoutine.java
+++ src/de/cpgaertner/edu/inf/api/routine/InteractionRoutine.java
@@ -1,8 +1,16 @@
 package de.cpgaertner.edu.inf.api.routine;
 
+import de.cpgaertner.edu.inf.api.adapter.Adapter;
 import de.cpgaertner.edu.inf.api.parsing.CommandSystemManager;
 
+import java.io.IOException;
+
 public abstract class InteractionRoutine implements Routine {
+
+    public enum Answer {
+        ONE, TWO, ABORT
+    }
+
     @Override
     public String getPrompt() {
         return null;
@@ -11,5 +19,29 @@ public abstract class InteractionRoutine implements Routine {
     @Override
     public CommandSystemManager getCommandSystemManager() {
         return null;
+    }
+
+    protected Answer askQuestion(String question, String answer1, String answer2, Adapter adapter) throws IOException {
+        String res;
+        while (true) {
+            res = adapter.read("[" + answer1 + "/" + answer2 + "/abort]");
+
+            if (res.equalsIgnoreCase(answer1)) {
+
+                return Answer.ONE;
+
+
+            } else if (res.equalsIgnoreCase(answer2)) {
+
+                return Answer.TWO;
+
+            } else if (res.equalsIgnoreCase("abort")) {
+
+                return Answer.ABORT;
+
+            } else {
+                adapter.send("Please choose one of the possible answers!");
+            }
+        }
     }
 }
--- src/de/cpgaertner/edu/inf/games/datacenter/level/groundfloor/location/DoorLocation.java
+++ src/de/cpgaertner/edu/inf/games/datacenter/level/groundfloor/location/DoorLocation.java
@@ -1,11 +1,28 @@
 package de.cpgaertner.edu.inf.games.datacenter.level.groundfloor.location;
 
+import de.cpgaertner.edu.inf.api.adapter.Adapter;
+import de.cpgaertner.edu.inf.api.command.Command;
 import de.cpgaertner.edu.inf.api.level.BaseLocation;
+import de.cpgaertner.edu.inf.api.level.Item;
+import de.cpgaertner.edu.inf.api.level.player.InsufficientInventorySpaceException;
+import de.cpgaertner.edu.inf.api.level.player.Player;
+import de.cpgaertner.edu.inf.api.routine.InteractionRoutine;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.io.IOException;
 
 public class DoorLocation extends BaseLocation {
 
-    public class Key {}
+    @Data
+    @AllArgsConstructor
+    public class Key implements Item {
+
+        protected String name;
+
+    }
 
 
     @Getter protected boolean open;
@@ -14,14 +31,80 @@ public class DoorLocation extends BaseLocation {
 
     public Key generateKey(boolean force) {
         if (key == null || force) {
-            return this.key = new Key();
+            return this.key = new Key("Key <> " + getName() + "");
         }
 
         throw new IllegalStateException("A key already exists for this door: <" + key + ">");
     }
 
+    public boolean lock() {
+        if (isOpen()) {
+            return false;
+        }
+        this.open = false;
+
+        return true;
+    }
+
     public boolean openWith(Key key) {
         return this.key == key && (open = true);
+    }
+
+    @AllArgsConstructor
+    public static class DefaultRoutine extends InteractionRoutine {
+
+        @Getter @Setter
+        protected DoorLocation door;
+
+        @Override
+        public boolean handle(Player player, Command cmd, Adapter adapter) throws IOException {
+
+            if (door.isOpen()) {
+
+                switch (askQuestion("Do you want to lock it?", "yes", "no", adapter)) {
+                    case ONE:
+                        Key key = null;
+                        try {
+                            key = door.generateKey(false);
+                        } catch (IllegalStateException e) {
+                            adapter.send("You need to provide a key to lock this door!");
+                            return false;
+                        }
+
+                        door.lock();
+
+                        try {
+                            player.getInventory().add(key);
+                        } catch (InsufficientInventorySpaceException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case TWO: /* falls through */
+                    default:
+                        adapter.send("Ok, then go ahead!");
+                }
+
+            } else {
+
+                switch (askQuestion("Do you want to open it?", "yes", "no", adapter)) {
+                    case ONE:
+                        adapter.send("Please choose a key!");
+                        adapter.send(player.getInventory().toString());
+                        String slot = adapter.read("slot #:");
+                        adapter.send("Key opening is not implemented yet... Sorry!");
+                        break;
+                    case TWO: /* falls through */
+                    default:
+                        adapter.send("Ok, then go ahead!");
+
+                }
+
+
+
+            }
+
+            return false;
+        }
     }
 
 }
